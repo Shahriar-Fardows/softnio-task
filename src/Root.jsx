7adc14650e9a93d29a97650cb7156c45ce774bf1
Shahriar Fardows <shahriarfardows@gmail.com>
2024-11-13 23:36:13 +0600

    first commit

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import NavbarIcons from "./Components/Shared/NavbarIcons/NavbarIcons";
 
 const Root = () => {
     return (
         <div>
+            <NavbarIcons/>
             <Outlet/>
         </div>
     );
